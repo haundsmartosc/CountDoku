4d5a27c26169a2c87668a1ba3ec37bc9d6b18718
haundsmartosc <haund@smartosc.com>
2025-01-13 16:18:51 +0700

    initial commit

---
+++ CountDoku/CountDokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDokuApp.swift
+//  CountDoku
+//
+//  Created by Nguyễn Đức Hậu on 13/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CountDokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
